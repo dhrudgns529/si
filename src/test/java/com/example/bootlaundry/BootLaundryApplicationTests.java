6d6602ffb44fe5e3bd5c2db566189319f4fe2f80
dhrudgns529 <dhrudgns529@naver.com>
2023-04-05 23:16:26 +0900

    부트

---
+++ src/test/java/com/example/bootlaundry/BootLaundryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootlaundry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLaundryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
